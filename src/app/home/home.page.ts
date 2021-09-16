e5595768d3417e505672bb5a123b3df5fa9c56a3
Isabella <isabella.moreno.bc@gmail.com>
2021-09-16 20:57:13 -0300

    se elimino lo de la animacion que no funciono :c

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -15,12 +15,8 @@ export class HomePage implements AfterViewInit{
   options:AnimationOptions = {
     path:'assets/car.json'
   }
-  
-  //ejemplo
-  anim: Animation;
-  @ViewChild('bienvenida', {static: false }) bienvenida: ElementRef;
 
-  constructor(private activeroute: ActivatedRoute, private router: Router, private animationCtrl: AnimationController) {
+  constructor(private activeroute: ActivatedRoute, private router: Router) {
     // Se llama a la ruta activa y se obtiene sus parametros mediante una subscripcion
     this.activeroute.queryParams.subscribe(params => { // Utilizamos lambda
       if (this.router.getCurrentNavigation().extras.state) { // Validamos que en la navegacion actual tenga extras
@@ -32,16 +28,6 @@ export class HomePage implements AfterViewInit{
   
   //ejemplo
   ngAfterViewInit(){
-    this.anim = this.animationCtrl.create();
-    this.anim
-    .addElement(this.bienvenida.nativeElement)
-    .duration(2000)
-    .keyframes([
-      { offset: 0, transform: 'scale(1)' },
-      { offset: 0.5, transform: 'scale(1.5)' },
-      { offset: 1, transform: 'scale(1)' }
-    ])
-    .iterations(Infinity)
   }
 
 }
