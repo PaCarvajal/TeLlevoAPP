d8fcce0314e70e66fb29dbe721ed8c52323e406e
Isabella <isabella.moreno.bc@gmail.com>
2021-11-05 23:37:01 -0300

    cambios mini :c

--- src/app/services/bdlocal.service.ts
+++ src/app/services/bdlocal.service.ts
@@ -32,37 +32,29 @@ export class BDlocalService {
     */
   guardarUsuario(username: string, password: string) {
     //Instrucción LAMBDA
-    const existeU= this.userlist.find(u=>u.strUser===username);
-    if(!existeU) {
-      const existeP= this.userlist.find(p=>p.strPass===password);
-      if(!existeP) {
+    const existe= this.userlist.find(u=>u.strUser===username);
+    if(!existe) {
         this.userlist.unshift({strUser: username, strPass: password})
         this.storage.set('usuario', this.userlist)
         console.log('Guardado');
-      } else {
+    } else {
         console.log('ya existe');
-      }
     }
   }
   /*Este código no funciona, no es capaz de retornar nada desde la base de datos :c
    *
   */
-  autenticarUsuario(username: string, password: string) {
-    console.log('entrando a autenticar con '+username+' '+password)
-    const existeU = this.userlist.find(u=>u.strUser===username);
-    const existeP= this.userlist.find(p=>p.strPass===password);
-    if(existeU) {
-      console.log('usuario encontrado')
-      if(existeP) {
-        console.log('password encontrada')
-        console.log('valida');
-        return true;
-      } else {
-        console.log('no valida');
-        return false;
-      }
+  autenticarUsuario(username: string) {
+    console.log('entrando a autenticar con '+username)
+    const existe = this.userlist.find(c=>c.strUser===username);
+    if(!existe) {
+      console.log('no encuentra usuario');
+      return false;
     } else {
-      console.log('fallo')
+      console.log('usuario encontrado')
+      return true;
     }
   }
+
+
 }
